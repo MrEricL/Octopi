0dea07c880f9517cc9535342e4f4ed2917c61261
Gillgamesh <ggill@stuy.edu>
2017-04-03 10:11:44 -0400

    Attempted to implement iterator

--- DLDeque.java
+++ DLDeque.java
@@ -1,5 +1,6 @@
 import java.util.NoSuchElementException;
-public class DLDeque<T> implements Deque<T>, Queue<T>{
+import java.util.Iterator;
+public class DLDeque<T> implements Deque<T>, Queue<T>, Iterable<T>{
 
     DLNode<T> _first;
     DLNode<T> _last;
@@ -40,12 +41,12 @@ public class DLDeque<T> implements Deque<T>, Queue<T>{
     public T removeFirst(){
 
         if (_size==0) throw new NoSuchElementException(); //throw error if necessary
-        T ret = _last.getValue();
-	
-        if (_size==1){	    
-            _first=_last;	    
+        T ret = _first.getValue();
+
+        if (_size==1){
+            _first=_last;
         }
-	
+
 
 	else {
         //else, just set front to it's next element
@@ -62,11 +63,11 @@ public class DLDeque<T> implements Deque<T>, Queue<T>{
 
         if (_size==0) throw new NoSuchElementException(); //throw error if necessary
         T ret = _last.getValue();
-	
-        if (_size==1){	    
-            _first=_last;	    
+
+        if (_size==1){
+            _first=_last;
         }
-	
+
 
 	else {
         //else, just set front to it's next element
@@ -109,6 +110,11 @@ public class DLDeque<T> implements Deque<T>, Queue<T>{
     public void enqueue(T ob) {addLast(ob);}
     public boolean isEmpty() {return _size==0;}
     public T peekFront() {return getFirst();}
+    //ITERATOR:
+    public Iterator<T> iterator()
+    {
+    return new MyIterator();
+    }
 
 
     //main method for testing:
@@ -126,12 +132,100 @@ public class DLDeque<T> implements Deque<T>, Queue<T>{
         System.out.println(q);
         q.addFirst("A");
 	System.out.println(q); //apple
-	//	System.out.println(q.getLast());	
+	//	System.out.println(q.getLast());
 	System.out.println(q.removeLast());
 	System.out.println(q);
 	System.out.println(q.removeFirst());
-	System.out.println(q);	
+	System.out.println(q);
+    for (String s: q) System.out.println(s);
     }
+
+
+
+
+    private class MyIterator implements Iterator<T>
+    {
+
+    private DLNode<T> _dummy; // dummy node to tracking pos
+    private boolean _okToRemove; //flag indicates next() was called
+
+    //constructor
+    public MyIterator()
+    {
+        //place dummy node in front of head
+            //...other housekeeping chores?
+            /* YOUR IMPLEMENTATION HERE */
+        _dummy = new DLNode<T>(null,null,_first);
+        _okToRemove = false;
+    }
+
+    //--------------v  Iterator interface methods  v-------------
+    //return true if iteration has more elements.
+    public boolean hasNext()
+    {
+            /* YOUR IMPLEMENTATION HERE */
+        return _dummy.getNext() != null;
+    }
+
+
+    //return next element in this iteration
+    public T next()
+    {
+            /* YOUR IMPLEMENTATION HERE */
+        _dummy = _dummy.getNext();
+        if (_dummy == null) throw new NoSuchElementException("No such element");
+        //not seen before, thanks Mr. Brown.
+        _okToRemove = true;
+        //you know that there is an item that you can remove here. Haf fun
+        return _dummy.getCargo();
+    }
+
+
+    //return last element returned by this iterator (from last next() call)
+    public void remove()
+    {
+        if ( ! _okToRemove )
+        throw new IllegalStateException("must call next() beforehand");
+        _okToRemove = false;
+
+        //If removing only remaining node...
+        //maintain invariant that _dummy always points to a node
+        //   (...so that hasNext() will not crash)
+
+            /* YOUR IMPLEMENTATION HERE */
+        if (_dummy == null) {
+        throw new IllegalStateException("No nodes available, add elements to your list"); //RECHECK THIS LATER NOT SURE
+        }
+
+
+        //if removing first node...
+            /* YOUR IMPLEMENTATION HERE */
+        if (_dummy.equals(_first)){
+            //set the node after the head to no longer point back to the head
+            _first.getNext().setLast(null);
+            _first = _first.getNext();
+        }
+        //if removing last node...
+            /* YOUR IMPLEMENTATION HERE */
+        else if (_dummy.equals(_last)){
+            //set the node before the tail to no longer refer back to the tail
+            _last.getLast().setNext(null);
+            _last = _last.getLast();
+        }
+        //if removing an interior node...
+        else {
+            //get the items before and after the node to refer to eachother rather than to the no longer existent node
+            _dummy.getNext().setLast(_dummy.getLast());
+            _dummy.getLast().setNext(_dummy.getNext());
+            _dummy = _dummy.getNext();
+        }
+            /* YOUR IMPLEMENTATION HERE */
+
+        _size--; //decrement size attribute of outer class LList
+    }
+    //--------------^  Iterator interface methods  ^--------------
+    }//*************** end inner class MyIterator ***************
+
 
 
 
