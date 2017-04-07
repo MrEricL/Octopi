d54301c361c1779f53951c294fd70f9484c49cc6
TakingTheL <yliu6@stuy.edu>
2017-04-06 20:53:30 -0400

    added DequeTester and renamed old one

--- DequeTester2.java
+++ DequeTester2.java
@@ -8,7 +8,7 @@ import java.util.Iterator;
 public class DequeTester<T>{
 
     public static void main(String[] args){
-	Deque<String> DeT = new ALDeque<String>();
+	Deque<String> DeT = new DLDeque<String>();
 
 	  
 	System.out.println("\nnow printing out queue..."); 
@@ -27,7 +27,7 @@ public class DequeTester<T>{
 	DeT.addLast("Side");
 
 	// testing add, adds the specified element into the queue, returns true if successful, ArrayLists don't have capacities
-	DeT.add("newItem");
+	//DeT.add("newItem");
 	System.out.println("\nadding to the front");
 	//testing addFirst, adds specified element at the front of the deque
 	DeT.addFirst("frontItem");
@@ -35,24 +35,27 @@ public class DequeTester<T>{
 	System.out.println("\nnow printing out queue..."); 
 	System.out.println( DeT ); //for testing toString()...
 
+	/*
 	//Tests contains, returns true if the value is in the arrayList
 	System.out.println("\nchecking contains method");
 	System.out.print("Contains War?: ");
 	System.out.println( DeT.contains("War")); // returns true
 	System.out.print("Contains fool?: ");
 	System.out.println( DeT.contains("fool")); // returns false
+	*/
 
-	
-	System.out.println("\nTest descendingIterator, Returns iterator");
-	System.out.println(DeT.descendingIterator()); //returns the iterator
+	/*
+		System.out.println("\nTest descendingIterator, Returns iterator");
+		System.out.println(DeT.descendingIterator()); //returns the iterator
 
 	System.out.println("\nUsing descendingIterator");
 	for(Iterator itr = DeT.descendingIterator(); itr.hasNext();){
 	    System.out.print(itr.next() + ", "); // prints out the queue elements in reverse order
 	}
+	*/
 
-	System.out.println("Tests element, Retrieves the head of the queue");
-	System.out.println(DeT.element()); //returns Lord
+	//	System.out.println("Tests element, Retrieves the head of the queue");
+	//	System.out.println(DeT.element()); //returns Lord
 	System.out.println("Tests getFirst, retrieves the first element of the queue");
 	System.out.println(DeT.getFirst()); // returns Lord
 	System.out.println("Tests getLast, retrieves the last element of the queue");
@@ -66,7 +69,7 @@ public class DequeTester<T>{
 	    System.out.print(itr.next() + ", "); // prints out the queue elements in proper order
 	}
 	
-	
+	/*	
 	System.out.println("Testing offer");
 	System.out.println(DeT.offer("newThing"));
 	
@@ -77,8 +80,9 @@ public class DequeTester<T>{
 	System.out.println(DeT.offerLast("offerLastThing"));
 	
 	System.out.println(DeT);
+	*/
 	
-
+	/*
 	System.out.println("Peeking Front and end");
 	System.out.println(DeT.peekFirst()); // returns anotherNewThing
 	System.out.println(DeT.peekLast()); //returns offerLastThing
@@ -93,6 +97,7 @@ public class DequeTester<T>{
 	
 	System.out.println(DeT);
 	
+       
 	System.out.println("Testing peek, Retrieves the front of the Deque");
 	System.out.println(DeT.peek()); //returns Lord
 
@@ -105,9 +110,9 @@ public class DequeTester<T>{
 	System.out.println("WARNING ONLY FOR STACKS");
 	System.out.print("Successful: ");
 	System.out.println(DeT.push("stackpush"));
-
-	System.out.println("Testing remove, Removing...the head");
-	System.out.println(DeT.remove()); //returns Lord
+	*/
+	//	System.out.println("Testing remove, Removing...the head");
+	//System.out.println(DeT.remove()); //returns Lord
 
 	System.out.println("Testing removeFirst, Removing... the head");
 	System.out.println(DeT.removeFirst()); //returns War
@@ -115,7 +120,7 @@ public class DequeTester<T>{
 	System.out.println("Testing removeLast, Removing... the end");
 	System.out.println(DeT.removeLast()); //Returns other
 
-	System.out.println(Det);
+	System.out.println(DeT);
 
 	
 	System.out.println("\nnow adding seven times..."); 
@@ -127,6 +132,8 @@ public class DequeTester<T>{
 	DeT.addLast("f");
 	DeT.addLast("a");
 
+	System.out.println(DeT);
+	/*
 	System.out.println("removing the first occurence in the deque");
 	System.out.print("returns true/false if removed: ");
 	System.out.println(DeT.removeFirstOccurrence("a")); // returns true
@@ -142,7 +149,8 @@ public class DequeTester<T>{
 	System.out.println( DeT.pollLast());
 	System.out.println(DeT.pollLast());
 	System.out.println(DeT.pollLast());
-
+	*/
+	/*
 	System.out.println("Testing Exceptions and conditions where the queue is empty");
 	System.out.println("\nDequeuing from empty queue should yield null...");
 	System.out.println( DeT.pollLast() );
@@ -153,6 +161,7 @@ public class DequeTester<T>{
 	System.out.println(DeT.peek());
 	System.out.println(DeT.peekFirst());
 	System.out.println(DeT.peekLast());
+	*/
 	
 
 
